b589152702f29efbc81bc43e5803aac41442cc91
Tim Doerzbacher <tim@tim-doerzbacher.com>
2018-05-20 20:06:54 -0400

    Add basic REST endpoints to control movement

--- src/Controller/CommandController.php
+++ src/Controller/CommandController.php
@@ -19,4 +19,25 @@ class CommandController extends Controller
     $output = substr($output, 2);
     return new Response($output);
   }
+
+  public function rotate($d)
+  {
+    error_log("rotate($d)");
+    exec(sprintf('catpoo_controller rotate %d', $d), $output, $result);
+    return new Response(implode("\n", $output));
+  }
+
+  public function translate($x, $y)
+  {
+    error_log("translate($x, $y)");
+    exec(sprintf('catpoo_controller translate %d %d', $x, $y), $output, $result);
+    return new Response(implode("\n", $output));
+  }
+
+  public function reset()
+  {
+    error_log("reset()");
+    exec('catpoo_controller reset', $output, $result);
+    return new Response(implode("\n", $output));
+  }
 }
